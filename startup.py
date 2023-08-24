43f59268871b910670a55b5548074aa21d5d3a7c
Ben McCalmon <benmccalmon@gmail.com>
2023-08-23 23:04:30 -0400

    debug: testing close app

--- startup.py
+++ startup.py
@@ -4,7 +4,11 @@ import tkinter as tk
 def main():
     root = tk.Tk()
 
+    def close_app():
+        root.destroy()
+
     tk.Label(root, text="Footy").pack()
+    tk.Button(root, text="Close", command=close_app).pack()
 
     def set_fullscreen():
         root.attributes("-fullscreen", True)
