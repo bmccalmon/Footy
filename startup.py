130249262b6e56d8a21b5f21b65278505dfbe35c
Ben McCalmon <benmccalmon@gmail.com>
2023-08-23 23:01:02 -0400

    debug: testing fullscreen

--- startup.py
+++ startup.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+import tkinter as tk
 
 def main():
-    print("RUNNING FOOTY")
+    root = tk.Tk()
+
+    tk.Label(root, text="Footy").pack()
+
+    def set_fullscreen():
+        root.attributes("-fullscreen", True)
+
+    root.after(100, set_fullscreen)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
